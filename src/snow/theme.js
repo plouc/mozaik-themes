6bbbad2dfdac310b8e441f0ca6d5a15c47d835d2
Raphael Benitte <benitte@ekino.com>
2016-12-22 13:03:33 +0900

    chore(cleanup): remove old themes config

--- src/snow/theme.js
+++ src/snow/theme.js
@@ -6,6 +6,7 @@ import widgetBody      from './WidgetBody.css'
 import widgetLabel     from './WidgetLabel.css'
 import widgetList      from './WidgetList.css'
 
+
 const theme = {
     root: Mozaik.root,
     dashboardHeader,
@@ -17,75 +18,3 @@ const theme = {
 }
 
 export default theme
-
-
-
-/*
-.time__clock
-  &__outer-circle
-    border-color #d3dfe8
-    background-color #f1f6f7
-    box-shadow 0 0 30px rgba(255, 255, 255, 0.75) inset
-
-  &__inner-circle
-    background #c5d2db
-
-  &__indicator
-    display none
-
-  &__hand
-    background-color #c5d2db
-    &--seconds
-      background-color #a9dca9
-
-const snowTheme = {
-    colors: {
-        background: '#ebf0f1',
-        text:       '#333',
-        textMute:   '#999',
-        unknown:    '#d3dfe8',
-        success:    '#8ddb8d',
-        warning:    '#d1be65',
-        failure:    '#e37856',
-    },
-    fonts: {
-        default: `normal normal 400 1.6vmin/3vmin 'Open Sans', sans-serif`,
-    },
-    settings: {
-        shadow: '0 1px 2px rgba(0, 0, 0, 0.15)',
-    },
-    notifications: {
-        bgColor:   '#fff',
-        textColor: '#333',
-        padding:   '0.8vmin 1.6vmin',
-        shadow:    '0 1px 2px rgba(0, 0, 0, 0.15), 0 4px 7px rgba(0, 0, 0, 0.15)',
-        overrides: {},
-    },
-    card: {
-        bgColor: '#fff',
-        shadow:  '0 10px 10px rgba(0, 0, 0, 0.15)',
-    },
-    table: {
-        cell: {
-            head: {
-                overrides: {
-                    borderBottom: '1px solid #ddd',
-                },
-            },
-        },
-    },
-    charts: {
-        axis: {
-            textColor:      '#777',
-            fontSize:       '1.3vmin',
-            tickColor:      '#777',
-            legendColor:    '#777',
-            legendFontSize: '1.3vmin',
-        },
-        grid: {
-            stroke:          '#aaa',
-            strokeDasharray: '1,3',
-        },
-    },
-}
-*/
--- src/sunny/theme.js
+++ src/sunny/theme.js
@@ -6,6 +6,7 @@ import widgetBody      from './WidgetBody.css'
 import widgetLabel     from './WidgetLabel.css'
 import widgetList      from './WidgetList.css'
 
+
 const theme = {
     root: Mozaik.root,
     dashboardHeader,
@@ -17,68 +18,3 @@ const theme = {
 }
 
 export default theme
-
-
-
-/*
-theme-yellow = {
-  // DASHBOARD
-  dashboard-header-txt-color     : #453d3a,
-}
-
-.time__clock {
-  background-color: themes[yellow][widget-header-bg-color];
-}
-.time__clock__outer-circle {
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15) inset
-  border-color: themes[yellow][widget-header-txt-color];
-  background-color: themes[yellow][widget-bg-color];
-}
-.time__clock__inner-circle,
-.time__clock__hande {
-  background-color: themes[yellow][widget-header-icon-color];
-}
-.time__clock__brand {
-  //color: darken(themes[yellow][widget-header-bg-color], 10);
-}
-
-const sunnyTheme = {
-    colors: {
-        background: '#e5dabe',
-        text:       '#444',
-        textMute:   '#c0ab7f',
-        unknown:    '#c0ab7f',
-        success:    '#4eb6a3',
-        warning:    '#d1be65',
-        failure:    '#ff9176',
-    },
-    settings: {
-        shadow: '0 1px 2px rgba(0, 0, 0, 0.15)',
-    },
-    card: {
-        bgColor: '#ede2c6',
-        shadow:  '0 10px 10px rgba(0, 0, 0, 0.15)',
-    },
-    table: {
-        cell: {
-            head: {
-                overrides: {
-                    borderBottom: '1px solid #e6d280',
-                },
-            },
-        },
-    },
-    charts: {
-        axis: {
-            fontSize:       '1.3vmin',
-            textColor:      '#735e39',
-            legendFontSize: '1.3vmin',
-            legendColor:    '#735e39',
-        },
-        grid: {
-            stroke:          '#dfcea6',
-            strokeDasharray: 'none',
-        },
-    },
-}
-*/
